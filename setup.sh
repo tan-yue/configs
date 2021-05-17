a13df86479a9e396889fb8ec87bab25c3da4dabf
Yue Tan <yuetan@cs.princeton.edu>
2021-05-17 14:01:31 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # start dockerd
-sudo groupadd docker
-sudo service docker start
-sudo usermod -aG docker $USER
+#sudo groupadd docker
+#sudo service docker start
+#sudo usermod -aG docker $USER
 
 # add cgroup
-sudo mount -o remount,ro -t cgroup /sys/fs/cgroup
-sudo mkdir /sys/fs/cgroup/cpu/firecracker
+#sudo mount -o remount,ro -t cgroup /sys/fs/cgroup
+#sudo mkdir /sys/fs/cgroup/cpu/firecracker
 
 # setup SSD
 if [ "$1" = "c220g2" ]; then
